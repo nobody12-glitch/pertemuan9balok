617e8ff60f16354912b94e07a40ee0d22e050cd5
nobody12-glitch <blacksentinel6@gmail.com>
2023-07-05 20:01:52 +0700

    using javanetbeans

---
+++ src/pertemuan9balok/Pertemuan9Balok.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan9balok;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Mahasiswa
+ */
+public class Pertemuan9Balok {
+
+    private double panjang;
+    private double lebar;
+    private double tinggi;
+    
+    public double getPanjang() {
+        return panjang;
+    }
+}
+ 
